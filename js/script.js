9d7c1d2749d3c28873e1f86554f57b67c19c7683
Mathias <mathias.grandin@gmail.com>
2020-07-08 16:23:14 +0200

    Esercizio corretto

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 var mail = ["ab@gmail.com", "bc@gmail.com", "cd@gmail.com", "de@gmail.com", "ef@gmail.com", "fg@gmail.com", "gh@gmail.com", "hi@gmail.com"];
+
 var insert = prompt("Inserisci la tua mail")
+
+var control = false
 
 for (var i = 0; i < mail.length; i++) {
   if (insert == mail[i]) {
-    alert("Accesso consentito");
-    break
+    control = true;
   }
-  else {
-    alert("Accesso negato");
-    continue
-  }
+}
+
+if (control == true) {
+  alert("Accesso consentito");
+ }
+else {
+  alert("Accesso negato");
 }
